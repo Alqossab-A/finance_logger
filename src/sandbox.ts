944949d7e0cb629f719d986b3529057a6e9d0d80
Ahmed <alqossaba@gmail.com>
2024-03-12 05:51:37 -0400

    functions

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,3 +1,23 @@
-let greet = () => {
-	console.log('hello, world');
-}
+let greet: Function;
+
+greet = () => {
+  console.log("hello");
+};
+
+// this will throw an error for c not being used
+/*function add = (a: number, b: number, c?: number | string) {
+  console.log(a + b)
+}*/
+
+const add = (a: number, b: number, c?: number | string): void => {
+  console.log(a + b);
+};
+
+add(5, 10);
+
+const minus = (a: number, b: number): number => {
+  return a + b;
+};
+
+let result = minus(10, 7);
+
