46dc8ae7daf025526cc3322a9c391984a7cd232a
Ahmed <alqossaba@gmail.com>
2024-03-14 05:43:26 -0400

    function signatures

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,10 +1,22 @@
-type StringOrNum = string | number;
-type objWithName = { name: string, uid: StringOrNum };
+let greet: (a: string, b: string) => void;
 
-const logDetails = (uid: StringOrNum, item: string) => {
-  console.log(`${item} has uid of ${uid}`);
+// This follows the signature above that is why there is no error
+greet = (name: string, greeting: string) => {
+  console.log(`${name} says ${greeting}`);
 }
 
-const greet = (user: objWithName) => {
-  console.log(`${user.name} says hello`);
+let calc: (a: number, b: number, c: string) => number;
+
+calc = (numOne: number, numTwo: number, action: string) => {
+  if (action === 'add') {
+    return numOne + numTwo;
+  } else {
+    return numOne - numTwo;
+  }
+}
+
+let logDetails: (obj: { name: string, age: number }) => void;
+
+logDetails = (ninja: { name: string, age: number }) => {
+  console.log(ninja);
 }
