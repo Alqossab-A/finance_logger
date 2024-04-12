aceeb7c6f02cb1cb5dbd00290481461a6c801ad9
Ahmed <alqossaba@gmail.com>
2024-04-11 21:02:21 -0400

    3

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,4 @@
-// Classes
-class Invoice {
-  readonly client: string;
-  private details: string;
-  public amount: number;
-
-  constructor(c: string, d: string, a: number) {
-    this.client = c;
-    this.details = d;
-    this.amount = a;
-  }
-
-  format() {
-    return `${this.client} owes $${this.amount} for ${this.details}`;
-  }
-}
+import { Invoice } from "./classes/invoice";
 
 const invOne = new Invoice('mario', 'work', 250);
 
