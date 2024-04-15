3714a21e0d3246d4fb407ab4a22d0114735809ea
Ahmed <alqossaba@gmail.com>
2024-04-15 17:46:32 -0400

    interfaces

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,30 @@
-import { Invoice } from "./classes/invoice";
+interface IsPerson {
+  name: string,
+  age: number,
+  speak(a: string): void;
+  spend(a: number): number;
+}
+
+//enforces rules on a variable
+const me: IsPerson = {
+  name: 'shaun',
+  age: 30,
+  speak(text: string): void {
+    console.log(text);
+  },
+  spend(amount: number): number {
+    console.log('I spent ', amount)
+    return amount;
+  }
+};
+
+const greetPerson = (person: IsPerson) => {
+  console.log('hello ', person.name, 'how are you today');
+}
+
+greetPerson(me);
+
+import { Invoice } from "./classes/invoice.js";
 
 const invOne = new Invoice('mario', 'work', 250);
 
